ee5d80ea3d88f6d03a0b91bf12c19145fe455b3a
chakravarthy-snps <59598633+chakravarthy-snps@users.noreply.github.com>
2020-01-07 15:30:55 +0530

    Update main.go

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ func main() {
    fmt.Println("printing SourceBranchName...")
    fmt.Println(os.Getenv("Build.SourceBranchName"))
    fmt.Println("-----")
-   fmt.Println(os.Getenv("$(Build.SourceBranchName)"))
+   fmt.Println(os.Getenv("$Build.SourceBranchName"))
    fmt.Println("____________________")
    fmt.Println("printing SourceBranch...")
    fmt.Println(os.Getenv("Build.SourceBranch"))
    fmt.Println("-----")
-   fmt.Println(os.Getenv("$(Build.SourceBranch)"))
+   fmt.Println(os.Getenv("$Build.SourceBranch"))
    fmt.Println(os.Getenv("POLARIS_SERVER_URL"))
 }
