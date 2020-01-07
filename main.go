8fe71b594067e554e121ab0ce2af9922471da522
chakravarthy-snps <59598633+chakravarthy-snps@users.noreply.github.com>
2020-01-07 15:36:42 +0530

    WITH CAPS

--- main.go
+++ main.go
@@ -7,13 +7,13 @@ import (
 )
 func main() {
    fmt.Println("printing SourceBranchName...")
-   fmt.Println(os.Getenv("Build.SourceBranchName"))
+   fmt.Println(os.Getenv("BUILD.SOURCEBRANCHNAME"))
    fmt.Println("-----")
-   fmt.Println(os.Getenv("$Build.SourceBranchName"))
+   fmt.Println(os.Getenv("$BUILD.SOURCEBRANCHNAME"))
    fmt.Println("____________________")
    fmt.Println("printing SourceBranch...")
-   fmt.Println(os.Getenv("Build.SourceBranch"))
+   fmt.Println(os.Getenv("BUILD.SOURCEBRANCH"))
    fmt.Println("-----")
-   fmt.Println(os.Getenv("$Build.SourceBranch"))
+   fmt.Println(os.Getenv("$BUILD.SOURCEBRANCH"))
    fmt.Println(os.Getenv("POLARIS_SERVER_URL"))
 }
